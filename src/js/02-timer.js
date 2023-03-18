ad1d063a91342c7afb210ce38b376d69ff14c1a6
daswift85 <daswift85@gmail.com>
2023-03-18 19:15:07 +0200

    4

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,11 +2,21 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-function convertMs(milliseconds) {
-  const seconds = Math.floor((milliseconds / 1000) % 60);
-  const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
-  const hours = Math.floor((milliseconds / 1000 / 60 / 60) % 24);
-  const days = Math.floor(milliseconds / 1000 / 60 / 60 / 24);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
@@ -15,43 +25,33 @@ function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
 }
 
-function startCountdown() {
-  const chosenDate = flatpickr.parseDate(document.querySelector('#datetime-picker').value);
-  const now = new Date();
-  const difference = chosenDate - now;
-  if (difference <= 0) return;
+function startCountdown(chosenDate, refs) {
+  let remainingTime = chosenDate - new Date();
+  if (remainingTime <= 0) return;
 
-  let remainingTime = difference;
-
-  const timer = setInterval(() => {
+  let timer = setInterval(() => {
     remainingTime -= 1000;
-
     if (remainingTime <= 0) {
       clearInterval(timer);
       remainingTime = 0;
     }
-
     const { days, hours, minutes, seconds } = convertMs(remainingTime);
 
-    document.querySelector('[data-days]').textContent = addLeadingZero(days);
-    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
-    document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
-    document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
+    refs.days.textContent = addLeadingZero(days);
+    refs.hours.textContent = addLeadingZero(hours);
+    refs.minutes.textContent = addLeadingZero(minutes);
+    refs.seconds.textContent = addLeadingZero(seconds);
 
   }, 1000);
-}
 
-function validateDate(selectedDates) {
-  const chosenDate = selectedDates[0];
-  const now = new Date();
-
-  if (chosenDate < now) {
-    Notiflix.Notify.warning("Please choose a date in the future");
-    document.querySelector('[data-start]').disabled = true;
-  } else {
-    Notiflix.Notify.success('Valid date selected');
-    document.querySelector('[data-start]').disabled = false;
-  }
+  refs.resetBtn.addEventListener('click', () => {
+    clearInterval(timer);
+    refs.days.textContent = '00';
+    refs.hours.textContent = '00';
+    refs.minutes.textContent = '00';
+    refs.seconds.textContent = '00';
+    refs.inputEl._flatpickr.setDate(new Date());
+  });
 }
 
 const options = {
@@ -59,12 +59,33 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose: validateDate,
+  onClose: function(selectedDates) {
+    const chosenDate = selectedDates[0];
+    const now = new Date();
+    if (chosenDate < now) {
+      Notiflix.Notify.warning("Please choose a date in the future");
+      refs.startBtn.disabled = true;
+    } else {
+      Notiflix.Notify.success('Valid date selected');
+      refs.startBtn.disabled = false;
+      refs.startBtn.addEventListener('click', () => startCountdown(chosenDate, refs));
+    }
+  },
 };
 
-flatpickr("#datetime-picker", options);
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  resetBtn: document.querySelector('[data-reset]'),
+  inputEl: document.querySelector('#datetime-picker'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
 
-document.querySelector('[data-start]').addEventListener('click', startCountdown);
+flatpickr(refs.inputEl, options);
+
+
 
 
 // import flatpickr from 'flatpickr';
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
+// Обрабатываем отправку формы
+form.addEventListener('submit', function(event) {
+  // Отменяем действие по умолчанию
+  event.preventDefault();
+  // Получаем значения полей
+  const delay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+  // Создаем промисы
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay + i * step)
+      .then(({ position, delay }) => {
+        notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
+
+// Функция для создания промиса
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
